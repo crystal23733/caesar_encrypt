501e7cce407facc2662505c66491dc28f6f2de96
Hoyeong Yoo <crystal23733@gmail.com>
2024-09-24 22:11:17 +0900

    :package:알파벳 대소문자 암호화 가능

--- encrypt.c
+++ encrypt.c
@@ -7,11 +7,11 @@ char encrypt(char enc);
 
 int main(void) {
   char str[100];
-  printf("대문자로 문자를 입력해주세요:");
+  printf("영어를 입력해주세요:");
   fgets(str, sizeof(str), stdin);
   str[strcspn(str, "\n")] = '\0';
   for (int i = 0; str[i] != '\0'; i++) {
-	  printf("%c", encrypt(str[i]));
+    printf("%c", encrypt(str[i]));
   }
   return 0;
 }
@@ -19,5 +19,9 @@ int main(void) {
 char encrypt(char enc) {
   if (enc >= 'A' && enc <= 'Z') {
     return ((enc - 'A' + SHIFT) % 26) + 'A';
+  } else if (enc >= 'a' && enc <= 'z') {
+    return ((enc - 'a' + SHIFT) % 26) + 'a';
+  } else {
+    return enc; // 알파벳 이외에는 그대로 반환
   }
 }
