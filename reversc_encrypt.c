972dda633da955caab4938c278e71df6152f1770
Hoyeong Yoo <crystal23733@gmail.com>
2024-09-24 22:03:12 +0900

    :sparkles:대문자 영어 복호화 기능 추가

---
+++ reversc_encrypt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+#define SHIFT 3
+
+char encrypt(char enc);
+
+int main(void) {
+  char str[100];
+  printf("해독할 암호를 입력해주세요:");
+  fgets(str, sizeof(str), stdin);
+  str[strcspn(str, "\n")] = '\0';
+  for (int i = 0; str[i] != '\0'; i++) {
+    printf("%c", encrypt(str[i]));
+  }
+  printf("\n");
+  return 0;
+}
+
+char encrypt(char enc) {
+  if (enc >= 'A' && enc <= 'Z') {
+    return ((enc - 'A' - SHIFT) % 26 + 'A');
+  }
+}
